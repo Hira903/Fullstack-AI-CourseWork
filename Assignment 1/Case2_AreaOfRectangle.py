8cd69840ded7b67fdf02d895ec3618763237283f
Hira903 <hiraidrees1702@gmail.com>
2025-08-19 00:31:42 +0500

    #Area Of Rectangle

---
+++ Assignment 1/Case2_AreaOfRectangle.py
@@ -0,0 +1,5 @@
+#   Calculate Area of a Rectangle
+
+Length = float(input("Enter length of rectangle : "))
+Width = float(input("Enter width of rectangle : "))
+print("Area of rectangle is :", Length * Width)
